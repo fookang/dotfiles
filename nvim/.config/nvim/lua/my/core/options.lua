11ede5a3de624bd650afdbe6558189aeb2075a5a
fookang <fookang3@gmail.com>
2026-04-09 01:19:22 +0800

    Add shiftround configuration

--- nvim/.config/nvim/lua/my/core/options.lua
+++ nvim/.config/nvim/lua/my/core/options.lua
@@ -6,8 +6,9 @@ opt.relativenumber = true
 opt.number = true
 
 -- tabs & indentation
-opt.tabstop = 4
+ opt.tabstop = 4
 opt.shiftwidth = 4
+opt.shiftround = true
 opt.expandtab = true
 opt.autoindent = true
 
