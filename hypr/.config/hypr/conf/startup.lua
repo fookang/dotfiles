c660797968984c4723ad03b43407303c2a540a57
fookang <fookang3@gmail.com>
2026-08-10 21:53:40 +0800

    Add hypridle to startup

--- hypr/.config/hypr/conf/startup.lua
+++ hypr/.config/hypr/conf/startup.lua
@@ -8,7 +8,7 @@
 -- Or execute your favorite apps at launch like this:
 --
 hl.on("hyprland.start", function () 
-  hl.exec_cmd("waybar & hyprpaper")
+  hl.exec_cmd("waybar & hyprpaper & hypridle")
 end)
 
 
